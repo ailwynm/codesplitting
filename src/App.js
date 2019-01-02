0c5d038fb7600c79726b3b49e1879b5aa19cbc4b
Ailwyn McGeoch <sneakyness@gmail.com>
2019-01-02 14:53:54 +0000

    More traditional codesplit but adds flash of white screen

--- src/App.js
+++ src/App.js
@@ -2,43 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Page1 from "./components/Page1";
+import AsyncComponent from "./components/AsyncComponent";
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
       route: "page1",
-      component: ""
+      component: null
     }
   }
 
   onRouteChange = (route) => {
-    //Code split
-    switch(route){
-      case "page1":
-        this.setState({route: route});
-      break;
-
-      case "page2":
-        import("./components/Page2").then((Page2) =>{
-          this.setState({route: route, component: Page2.default});
-        });
-      break;
-
-      case "page3":
-        import("./components/Page3").then((Page3) =>{
-          this.setState({route: route, component: Page3.default});
-        });
-      break;
-    }
+    this.setState({route: route});
   }
 
   render() {
     switch(this.state.route){
       case "page1":
       return <Page1 onRouteChange={this.onRouteChange} />
+      case "page2":
+        const AsyncPage2 = AsyncComponent(()=> import("./components/Page2"));
+        return <AsyncPage2 onRouteChange={this.onRouteChange} />
+      case "page3":
+        const AsyncPage3 = AsyncComponent(()=> import("./components/Page3"));
+        return <AsyncPage3 onRouteChange={this.onRouteChange} />
       default:
-      return <this.state.component onRouteChange={this.onRouteChange} />
     }
   }
 }
