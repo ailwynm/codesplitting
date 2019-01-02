f6ae6db5876105d364a462b580a8d80385576486
Ailwyn McGeoch <sneakyness@gmail.com>
2019-01-02 13:52:18 +0000

    Basic code split

--- src/App.js
+++ src/App.js
@@ -2,30 +2,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Page1 from "./components/Page1";
-import Page2 from "./components/Page2";
-import Page3 from "./components/Page3";
 
 class App extends Component {
-
   constructor(){
     super();
     this.state = {
-      route: "page1"
+      route: "page1",
+      component: ""
     }
   }
 
   onRouteChange = (route) => {
-    this.setState({route: route});
+    //Code split
+    switch(route){
+      case "page1":
+        this.setState({route: route});
+      break;
+
+      case "page2":
+        import("./components/Page2").then((Page2) =>{
+          this.setState({route: route, component: Page2.default});
+        });
+      break;
+
+      case "page3":
+        import("./components/Page3").then((Page3) =>{
+          this.setState({route: route, component: Page3.default});
+        });
+      break;
+    }
   }
 
   render() {
-    switch (this.state.route){
+    switch(this.state.route){
       case "page1":
-        return <Page1 onRouteChange={this.onRouteChange} />
-      case "page2":
-        return <Page2 onRouteChange={this.onRouteChange} />
-      case "page3":
-        return <Page3 onRouteChange={this.onRouteChange} />
+      return <Page1 onRouteChange={this.onRouteChange} />
+      default:
+      return <this.state.component onRouteChange={this.onRouteChange} />
     }
   }
 }
